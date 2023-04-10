db490d6fd9c212e3e270b69e0b2bf74bdb6b7f56
sangramsingh822 <ssangram111@gmail.com>
2023-04-10 15:21:22 +0530

    side bar responsive

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -1,17 +1,57 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import { FiMenu } from "react-icons/fi";
+import { RxCross2 } from "react-icons/rx";
+const Layout = ({ children }) => {
+  const [open, setopen] = useState(false);
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
 
-const Layout = ({children}) => {
-  return (<>
-  
-    <Header/>
-    <main className="flex bg-black text-white h-screen w-screen overflow-hidden">
-      <Sidebar />
-      <div className="flex flex-col flex-1">
-        <div className="flex-1  min-h-0 overflow-auto">{children}</div>
-      </div>
-    </main>
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (windowSize.width && windowSize.width < 640) {
+      setopen(false);
+    } else {
+      setopen(true);
+    }
+  }, [windowSize]);
+
+  return (
+    <>
+      <Header />
+
+      <main className="flex bg-black text-white h-screen w-screen overflow-hidden relative">
+        {open ? (
+          <div className ="absolute top-55 left-40 " onClick={()=>setopen(false)}>
+            <RxCross2 />
+          </div>
+        ) : (
+          <div className="absolute left-2" onClick={()=>setopen(true)}>
+            <FiMenu />
+          </div>
+        )}
+
+        {open ? <Sidebar /> : <></>}
+
+        <div className="flex flex-col flex-1 p-5">
+          <div className="flex-1  min-h-0 overflow-auto">{children}</div>
+        </div>
+      </main>
     </>
   );
 };
--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -5,7 +5,7 @@ import SidebarLink from "./SidebarLink";
 
 const Sidebar = () => {
   return (
-    <div className="flex flex-col bg-neutral-900 w-60 p-3 text-white md:w-80 lg:w-96">
+    <div className="flex flex-col bg-black w-60 p-3 text-white md:w-80 lg:w-96">
       <div className="flex-1 py-8 flex flex-col gap-0.5">
         {Links.map((item) => {
           return <SidebarLink key={item.key} item={item} />;
